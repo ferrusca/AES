7268cba68a625a8dda3bdbca6607907f60bcb760
JorgeLuis <estebanracing21@gmail.com>
2019-04-03 13:08:07 -0600

    Con un chingo de warnings

---
+++ aes.h
@@ -0,0 +1,12 @@
+typedef unsigned char B8;
+typedef unsigned short int B16;
+typedef unsigned long int B32;
+
+
+#define Nb 4
+#define debug 1
+
+B32 w[4*11]; //Without this, doesnt work :( 
+
+B16 Nk;
+B16 Nr;
